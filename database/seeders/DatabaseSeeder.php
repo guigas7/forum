f4d94401b596ba331b72c87542eb457b0657f2ab
Guilherme <jimhorton7@outlook.com>
2024-04-03 18:47:07 -0300

    Starts from chapter 2
    Signed-off-by: Guilherme <jimhorton7@outlook.com>

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $users = User::factory(10)->create();
+        $posts = Post::factory(200)
+            ->recycle($users)
+            ->create();
+        $comments = Comment::factory(100)
+            ->recycle($users, $posts)
+            ->create();
+
+        User::factory()
+            ->has(Post::factory(45))
+            ->has(Comment::factory(100)->recycle($posts))
+            ->create([
+            'name' => 'Guigas Teste',
+            'email' => 'guibasket97@gmail.com',
+        ]);
+    }
+}
