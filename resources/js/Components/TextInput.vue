ae630cd6737362b1a573e6aec2e9ede138323733
Guilherme <jimhorton7@outlook.com>
2024-05-21 10:12:22 -0300

    Ep 41
    Signed-off-by: Guilherme <jimhorton7@outlook.com>

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,11 +1,7 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import {onMounted, ref} from 'vue';
 
-defineProps({
-    modelValue: String,
-});
-
-defineEmits(['update:modelValue']);
+const model = defineModel();
 
 const input = ref(null);
 
@@ -15,14 +11,13 @@ onMounted(() => {
     }
 });
 
-defineExpose({ focus: () => input.value.focus() });
+defineExpose({focus: () => input.value.focus()});
 </script>
 
 <template>
     <input
         ref="input"
         class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
+        v-model="model"
     >
 </template>
